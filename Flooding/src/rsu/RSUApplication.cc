63112264bcb6cfa396a8c4faa79a72058285313a
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-06-17 14:56:59 +0000

    Corrigindo erro de ambiguidade do operador <=

--- Flooding/src/rsu/RSUApplication.cc
+++ Flooding/src/rsu/RSUApplication.cc
@@ -155,7 +155,7 @@ void RSUApplication::readDataFromFile() {
         outputQueue.push_back(videoInfo);
     }*/
 
-    for (int ID = 1; ID <= par("numberPackets"); ID++) {
+    for (int ID = 1; ID <= par("numberPackets").stringValue(); ID++) {
         MessageEntryInfo* videoInfo = new MessageEntryInfo;
         videoInfo->ID = ID;
         videoInfo->length = par("packetSize");
